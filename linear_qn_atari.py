b5cff750fe16579a1f0d50ebaae5088acafd9302
AlanPi1992 <xpi@andrew.cmu.edu>
2017-03-25 15:53:44 -0700

    add linear q-net

--- linear_qn_atari.py
+++ linear_qn_atari.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Run Atari Environment with DQN."""
+"""Run Atari Environment with Linear-QN."""
 import argparse
 import os
 import random
@@ -50,17 +50,11 @@ def create_model(window, input_shape, num_actions,
       The Q-model.
     """
     # Using tensorflow name scope
-    # Create a deep Q-network
+    # Create a linear Q-network
     with tf.name_scope(model_name):
         input_img = Input(shape = (window,) + input_shape) # Input shape = (4, 84, 84)
-        conv1 = Convolution2D(32, (8,8), strides=4, padding='same', activation='relu')(input_img)
-        # conv1 = Dropout(0.2)(conv1)
-        conv2 = Convolution2D(64, (4,4), strides=2, padding='same', activation='relu')(conv1)
-        # conv2 = Dropout(0.2)(conv2)
-        # conv2 = Convolution2D(64, (3,3), strides=1, padding='same', activation='relu')(conv2)
-        flat = Flatten()(conv2) # Flatten the convoluted hidden layers before full-connected layers
-        full = Dense(512, activation='relu')(flat)
-        out = Dense(num_actions)(full) # output layer has node number = num_actions
+        flat = Flatten()(input_img) # Flatten the convoluted hidden layers before full-connected layers
+        out = Dense(num_actions)(flat) # output layer has node number = num_actions
         model = Model(input = input_img, output = out)
     return model
 
@@ -106,7 +100,7 @@ def main():  # noqa: D103
     parser = argparse.ArgumentParser(description='Run DQN on Atari Breakout')
     parser.add_argument('--env', default='SpaceInvaders-v0', help='Atari env name')
     parser.add_argument(
-        '-o', '--output', default='deepQ', help='Directory to save data to')
+        '-o', '--output', default='linearQ', help='Directory to save data to')
     parser.add_argument('--seed', default=703, type=int, help='Random seed')
 
     args = parser.parse_args()
@@ -123,7 +117,7 @@ def main():  # noqa: D103
 
     # Create a Q network
     num_actions = env.action_space.n
-    q_net = create_model(4, (84, 84), num_actions, model_name='Deep_Q_Net_with_Replay_Memory_and_Target_Fixing')
+    q_net = create_model(4, (84, 84), num_actions, model_name='Linear_Q_Net_with_Replay_Memory_and_Target_Fixing')
     # print('======================== Keras Q-network model is created. =========================')
 
     # Initialize a preporcessor sequence object
